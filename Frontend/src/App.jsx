e4b902a723f50e005cc1f9188730a0e6eaf5af9d
Henok Asaye <henokasaye77@gmail.com>
2025-04-21 19:35:45 +0300

    add the Header and the footer

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import Header from './components/Header/header'
 import Fotter from './components/Footer/fotter'
 import './App.css'
 
 function App() {
   return (
-    <Header/>,
-    <Fotter/>
+    <div>
+        <Header/>
+        <Fotter/>
+    </div>
   )
 }
 
