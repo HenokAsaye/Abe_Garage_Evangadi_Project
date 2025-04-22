46cdc497d3c33b2f2401e03ff42f73c76adba596
Henok Asaye <henokasaye77@gmail.com>
2025-04-22 17:44:17 +0300

    add the about section for the home page

--- Frontend/src/components/Home/Home.jsx
+++ Frontend/src/components/Home/Home.jsx
@@ -1,16 +1,61 @@
 import React from 'react'
 import homeEngine from "../../assets/home-engine.jpg"
+import aboutPage from "../../assets/about-us.png"
+import aboutPage2 from "../../assets/about-us1.png"
 
 const Home = () => {
   return (
-    <div className='w-full relative'>
+    <div>
         {/* HeroSection */}
-        <div className='absolute pt-4 flex flex-col w-1/2 h-3/4 justify-center items-start'>
-            <span className='text-gray-50 font-semibold text-[15px] w-full md:px-20 md:py-6 flex items-end gap-2'>Working since 1992 <span className='inline-block w-16 h-[1.8px] bg-red-600'></span></span>
-            <span className='text-gray-50 font-bold text-2xl md:text-5xl md:px-20'>Tune Up Your Car for the next level</span>
+        <div className='w-full relative'>
+            <div className='absolute pt-4 flex flex-col w-1/2 h-3/4 justify-center items-start'>
+                <span className='text-gray-50 font-semibold text-[15px] w-full md:px-20 md:py-6 flex items-end gap-2'>Working since 1992 <span className='inline-block w-16 h-[1.8px] bg-red-600'></span></span>
+                <span className='text-gray-50 font-bold text-2xl md:text-5xl md:px-20'>Tune Up Your Car for the next level</span>
+            </div>
+            <img className="w-full object-cover" src={homeEngine} alt="engine"/>
         </div>
-        <img className="w-full object-cover " src={homeEngine} alt="engine"/>
+
+
+
+        {/* AboutSection */}
+
+        <div className='flex flex-col md:flex-row gap-4 md:gap-10 w-full items-center justify-center mt-10 mb-10 '>
+
+                {/* Image Section */}
+                <div className='flex gap-4 md:w-1/3 justify-center'>
+                    <img src={aboutPage} alt="About 1" className="w-40 md:w-64 object-cover rounded" />
+                    <img src={aboutPage2} alt="About 2" className="w-40 md:w-64 object-cover rounded" />
+                </div>
+
+                {/* Text Section */}
+                <div className='flex flex-col gap-4 md:w-1/3 justify-baseline'>
+                    <h2 className='textsm text-gray-800 '>Welcome to Our Workshop </h2>
+                    <p className='text-gray-700 text-sm'>
+                        <strong className='block mb-2 text-xl md:text-4xl'>We have 24 years of experience <span className='block bg-red-700 w-16 h-[2px] mt-2'></span></strong>
+                        Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward,
+                        a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution.
+                         User generated content in real-time will have multiple touchpoints for offshoring.
+                    </p>
+                    <p className='text-gray-700 text-sm'>
+                        Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing.
+                    </p>
+                    <button className='bg-red-700 text-white px-6 py-2 rounded w-fit hover:scale-110 hover:cursor-pointer'>
+                        About Us
+                    </button>
+                </div>
+        </div>
+
+
+
+        {/* ////////About Section End////// */}
+
+
+
+
+
+
     </div>
+
   )
 }
 
