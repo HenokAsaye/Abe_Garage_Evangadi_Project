cd7cee99525597fec574f9738265ee1c8e1853af
Henok Asaye <henokasaye77@gmail.com>
2025-04-22 16:33:20 +0300

    add the neccessary images in the asset folder

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import Header from './components/Header/header'
 import Fotter from './components/Footer/fotter'
+import Home from './components/Home/Home'
 import './App.css'
 
 function App() {
   return (
     <div>
         <Header/>
+        <Home/>
         <Fotter/>
     </div>
   )
